1a51775782f3d84a8bd919db9dec139fdc3c8891
WinstonWolf007 <ubguertin@gmail.com>
2021-10-22 19:02:01 -0400

    v4 [STABLE VERSION]

--- python_file_lang2/basic2.py
+++ python_file_lang2/basic2.py
@@ -12,7 +12,6 @@ from ERROR import ERROR
 LETTERS = string.ascii_letters + "_"
 TT_VAR = {}
 
-
 ##########################################
 # MAIN CODE
 ##########################################
@@ -24,36 +23,52 @@ class Code:
 
     def CODE_LANG(self):
 
+        ERROR_CODE = False
+
         ########################################################################
         #                             VARIABLE                                 #
         ########################################################################
 
         match self.code:
-            case [type_, name, "=", value]:
+            case [type_, name, "=", *value]:
+
+                if type_[:-1] != "string":
+                    value = value[0]
 
                 if name[0] == "$":
                     for letters in name[1:]:
                         if letters not in LETTERS:
                             print(ERROR("SyntaxError", f"Expected '{letters}' is not in [{LETTERS}]"))
+                            ERROR_CODE = True
                 else:
                     for letters in name:
                         if letters not in LETTERS:
                             print(ERROR("SyntaxError", f"Expected '{letters}' is not in [{LETTERS}]"))
+                            ERROR_CODE = True
 
                 if type_[-1] == ':':
                     if type_[:-1] not in ['int', 'float', 'string', 'bool']:
                         print(ERROR("TypeError", f"'{type_}' is not exist in (int, float, string, bool)"))
+                        ERROR_CODE = True
                 else:
                     if type_ not in ['int', 'float', 'string', 'bool']:
                         print(ERROR("TypeError", f"'{type_}' is not exist in (int, float, string, bool)"))
+                        ERROR_CODE = True
 
                 if type_[-1] != ":":
                     print(ERROR("SyntaxError", f"is missing ':' to the end '{type_}'", "[type]:"))
+                    ERROR_CODE = True
 
                 if name[0] != "$":
                     print(ERROR("SyntaxError", "It missing '$' the start of the variable", '$[name]'))
+                    ERROR_CODE = True
 
-                if type_[:-1] in ['int', 'float', 'string', 'bool'] and type_[-1] == ':' and name[0] == "$":
+                if type_[:-1] == "string":
+                    if value[0][0] != '"' or value[-1][-1] != '"':
+                        print(ERROR("SyntaxError", f"It missing '\"\"' in type 'string'"))
+                        ERROR_CODE = True
+
+                if type_[:-1] in ['int', 'float', 'string', 'bool'] and type_[-1] == ':' and name[0] == "$" and ERROR_CODE == False:
 
                     if type_[:-1] == 'int':
                         try:
@@ -68,14 +83,8 @@ class Code:
                             print("probleme")
 
                     elif type_[:-1] == 'string':
-                        try:
-                            if value[0] == '"' and value[-1] == '"':
-                                TT_VAR[name[1:]] = [type_[:-1], str(value)]
-                            else:
-                                print(ERROR("ValueError", f"'{value}' cannot be an string type, he missing the \"\""))
-
-                        except:
-                            print(ERROR("ValueError", f"'{value}' cannot be an string type, he missing the '\"\"'"))
+                        if value[0][0] == '"' and value[-1][-1] == '"':
+                            TT_VAR[name[1:]] = [type_[:-1], " ".join(value)]
 
                     elif type_[:-1] == 'bool':
                         try:
@@ -108,40 +117,46 @@ class Code:
             #                             OPERATOR                                 #
             ########################################################################
             case [*ops]:
-                number = ops[0::2]
-                operator = ops[1::2]
-                resultSTR = ''
-                num = 0
-                oper = 0
-                bools = True
-                j = 0
+                if not ERROR_CODE:
+                    number = ops[0::2]
+                    operator = ops[1::2]
+                    resultSTR = ''
+                    num = 0
+                    oper = 0
+                    bools = True
+                    j = 0
 
-                for i in number:
-                    if TT_VAR.get(str(i[1:])) is None:
-                        j += 1
-                    else:
-                        if TT_VAR.get(str(i[1:]))[0] in ['float', 'int']:
-                            if TT_VAR.get(str(i[1:]))[0] == 'int':
-                                number[j] = int(TT_VAR.get(str(i[1:]))[1])
-                                j += 1
-                            else:
-                                number[j] = float(TT_VAR.get(str(i[1:]))[1])
-                                j += 1
-
-                try:
-                    for x in range(len(number) + len(operator)):
-                        if bools:
-                            resultSTR += str(number[num])
-                            bools = False
-                            num += 1
+                    for i in number:
+                        if TT_VAR.get(str(i[1:])) is None:
+                            j += 1
                         else:
-                            resultSTR += str(operator[oper])
-                            bools = True
-                            oper += 1
+                            if TT_VAR.get(str(i[1:]))[0] in ['float', 'int']:
+                                if TT_VAR.get(str(i[1:]))[0] == 'int':
+                                    number[j] = int(TT_VAR.get(str(i[1:]))[1])
+                                    j += 1
+                                else:
+                                    number[j] = float(TT_VAR.get(str(i[1:]))[1])
+                                    j += 1
+                            else:
+                                print(ERROR("CrachCodeError", f"Fatal error", "this bug"))
+                                ERROR_CODE = True
 
-                    result = eval(resultSTR)
-                    print('\033[34m' + str(result) + '\033[0m')
+                    try:
+                        if not ERROR_CODE:
+                            for x in range(len(number) + len(operator)):
+                                if bools:
+                                    resultSTR += str(number[num])
+                                    bools = False
+                                    num += 1
+                                else:
+                                    resultSTR += str(operator[oper])
+                                    bools = True
+                                    oper += 1
 
-                except:
-                    print(ERROR("ValueError",
-                                f"type '{str(TT_VAR.get(number[0][1:])[0])}' in {str(number[0])} is not 'int' or 'float'"))
+                            result = eval(resultSTR)
+                            print('\033[34m' + str(result) + '\033[0m')
+
+                    except:
+                        print(ERROR("ValueError", f"type '{str(TT_VAR.get(number[0][1:])[0])}' in {str(number[0])} is not 'int' or 'float'"))
+            case _:
+                print(ERROR("CommandError", f"{self.code} is not found !", "'help' command for vew all command"))
--- python_file_lang2/shell2.py
+++ python_file_lang2/shell2.py
@@ -1,5 +1,12 @@
 from basic2 import *
 
+print('\033[92m---> The Qwerdy Language is create by WinstonWolf007 <---'+"\n\t* [VERSION] => [5.0]\033[90m")
+print('\n')
 while True:
     txt = input("\033[90mQwerdy-shell>\033[0m")
-    Code(txt)
+    if txt.replace(" ", "") == "":
+        pass
+    elif txt.split()[0] == '//' or txt[0] == "/" and txt[1] == '/':
+        pass
+    else:
+        Code(txt)
