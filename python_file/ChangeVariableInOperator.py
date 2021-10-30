9a154abe0e9d78ec84ca2f52dcfdd584a3f6b27b
WinstonWolf007 <ubguertin@gmail.com>
2021-10-30 07:49:08 -0400

    v5.10 [UNSTABLE VERSION]
    create main import file

---
+++ python_file/ChangeVariableInOperator.py
@@ -0,0 +1,17 @@
+from data import Data
+
+class ChangeVariableForValue:
+
+    def __init__(self, values: list):
+        self.data = Data()
+        self.values = values
+
+    def changeData(self):
+        j = 0
+        for i in self.values:
+            if i[0] == '$':
+                dataVar = self.data.GET_var(i)
+                self.values[j] = dataVar
+                j += 1
+
+        return self.values
---
+++ python_file/__init__.py
@@ -0,0 +1,22 @@
+import basic2
+import ChangeVariableInOperator
+import checkType
+import condition
+import data
+import error
+import function
+import shell2
+import variable
+
+class __init__:
+
+    def __init__(self):
+        self.basic = basic2
+        self.changeVariableType = ChangeVariableInOperator
+        self.checkType = checkType
+        self.condition = condition
+        self.data = data
+        self.error = error
+        self.function = function
+        self.shell = shell2
+        self.variable = variable
