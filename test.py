37583cd4cc540f3c0491d2f9b156c802fe9cf51f
WinstonWolf007 <ubguertin@gmail.com>
2021-11-14 14:28:45 -0500

    mpdify

---
+++ test.py
@@ -0,0 +1,19 @@
+"""
+
+    Function code for stock in list
+
+"""
+
+# from python_file.data import *
+#
+# code = input(">").split()
+#
+# if code[0] == "FUNC:":
+#     run = True
+#     TT_FUNC[code[1][1:]] = []
+#     while run:
+#         code2 = input("...    ").split()
+#         TT_FUNC[code[1][1:]].append(code2)
+#
+#         if code2[0] == 'end;':
+#             run = False
